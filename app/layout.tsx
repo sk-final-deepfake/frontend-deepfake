a1e0a9684fa168ccc394fe959a835900ad5cf87f
kimini02 <kimini02@gmail.com>
2026-06-19 15:59:05 +0900

    루트 레이아웃 인증 provider 중복 import 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { AuthProvider } from '@/components/auth-provider'
 import { ThemeProvider } from '@/components/theme-provider'
-import { AuthProvider } from '@/components/auth-provider'
 import './globals.css'
 
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] })
